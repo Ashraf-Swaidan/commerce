d2c5b6badee842abb680382d2ae0ff58602697c0
Ashraf-Swaidan <ashraf.swaidan.10@gmail.com>
2024-10-22 22:19:35 +0300

    ah

--- commerce/settings.py
+++ commerce/settings.py
@@ -32,6 +32,7 @@ print(os.getenv('SECRET_KEY'))
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://auctions-rf5r.onrender.com']
 
 
 # Application definition
@@ -48,13 +49,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 
